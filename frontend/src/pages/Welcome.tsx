d4ccf73de0ff850c201f11eadfdf314a3976b213
dzakifadh <fadh.dzaki@gmail.com>
2023-01-14 07:28:24 +0700

    feat: :sparkles: Add welcome page

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,5 +1,21 @@
+import PencilIcon from "../components/icons/PencilIcon";
+
 const Welcome = () => {
-	return <div>Welcome</div>;
+	return (
+		<div className="flex h-screen items-center justify-center bg-dark text-white">
+			<div className="text-center">
+				<h1 className="text-3xl">
+					Welcome to <span className="font-bold">Notepad Web</span>
+				</h1>
+				<button
+					type="button"
+					className="mt-6 inline-flex items-center justify-center gap-2 rounded-lg bg-blue-600 py-3 px-6 duration-300 hover:bg-blue-700"
+				>
+					<PencilIcon /> New Note
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Welcome;
